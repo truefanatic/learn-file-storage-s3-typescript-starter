2b571319cd70689aa3682b4dfdf664bbb541a037
Kyrylo Garmash <kirillgarmash@gmail.com>
2025-10-07 14:30:56 -0600

    cache

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,11 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import path from "node:path";
+import { randomBytes } from "node:crypto";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -48,6 +50,47 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
   // TODO: implement the upload here
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if (file instanceof File) {
+    const MAX_UPLOAD_SIZE = 10 * 1024 * 1024;
+    if (file.size > MAX_UPLOAD_SIZE) {
+      throw new BadRequestError("Thumbnail file exceeds the maximum allowed size of 10MB");
+    }
+  } else {
+    throw new BadRequestError("Thumbnail file missing");
+  }
+  const mediaType = file.type;
+  // if (!mediaType) {
+  //   throw new BadRequestError("Missing Content-Type for thumbnail");
+  // }
+  if ((mediaType !== "image/jpeg") && (mediaType !== "image/png")) {
+     throw new BadRequestError("Thumbnail should be image image/jpeg or image/png");
+  }
 
-  return respondWithJSON(200, null);
+  const data = await file.arrayBuffer();
+  if (!data) {
+    throw new Error("Error reading file data");
+  }
+
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Couldn't find video");
+  }
+  if (video.userID != userID) {
+    throw new UserForbiddenError("Not authorized to update this video");
+  }
+
+
+  const ext = mediaType.split("/")[1];
+  const fileName = randomBytes(32).toString("base64url");
+  const filePath = path.join(cfg.assetsRoot, `${fileName}.${ext}`);
+  await Bun.write(filePath, data, { createPath: true });
+
+  videoThumbnails.set(fileName, { data, mediaType });
+  video.thumbnailURL = `http://localhost:${cfg.port}/assets/${fileName}.${ext}`;
+
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
